68e757aa7425244e6eee34af04c076fa7eaab4ff
Marius J Bauer <bauermariusjohannes@t-online.de>
2019-10-19 03:55:49 +0200

    result display

--- app/src/main/java/com/example/annuittenrechner/MainActivity.java
+++ app/src/main/java/com/example/annuittenrechner/MainActivity.java
@@ -1,51 +1,58 @@
 package com.example.annuittenrechner;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Button;
+
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     private Button AnnuitätButton;
-    private Button BerechnungsverlaufButton;
-    private EditText DarlehenssummeEingabe;
-    private EditText ZinssatzEingabe;
-    private EditText LaufzeitEingabe;
+    private EditText eTDarlehenssumme;
+    private EditText eTZinssatz;
+    private EditText eTLaufzeit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         AnnuitätButton = findViewById(R.id.AnnuitätButton);
-        BerechnungsverlaufButton = findViewById(R.id.BerechnungsverlaufButton);
-        DarlehenssummeEingabe = findViewById(R.id.DarlehenssummeEingabe);
-        ZinssatzEingabe = findViewById(R.id.ZinssatzEingabe);
-        LaufzeitEingabe = findViewById(R.id.LaufzeitEingabe);
-
         AnnuitätButton.setOnClickListener(this);
-        BerechnungsverlaufButton.setOnClickListener(this);
-
-        int DarlehensSumme;
-        int Zinssatz;
-        int Laufzeit;
-
-        int Annuität = 0;
-
     }
-    public void onClick(View view){
 
-            if(view == AnnuitätButton){
-                int DarlehensSumme = Integer.valueOf(DarlehenssummeEingabe.getText().toString());
-                int Zinssatz = Integer.valueOf(ZinssatzEingabe.getText().toString());
-                int Laufzeit = Integer.valueOf(LaufzeitEingabe.getText().toString());
-                int Annuität = DarlehensSumme * (Zinssatz*(1+Zinssatz)^Laufzeit)/((1+Laufzeit)^Laufzeit-1);
-            }
-            if(view == BerechnungsverlaufButton){
-            setContentView(R.layout.result_display);
-            }
+    protected double leseDarlehenssumme(){
+        double ds = Double.parseDouble(eTDarlehenssumme.getText().toString());
+        return ds;
+    }
+
+    protected int leseZinssatz(){
+        int zs = Integer.parseInt(eTZinssatz.getText().toString());
+        return zs;
+    }
+
+    protected int leseLaufzeit(){
+        int lz = Integer.parseInt(eTLaufzeit.getText().toString());
+        return lz;
+    }
+
+    protected double berechneAnnuität(double darlehenssumme, int zinssatz, int laufzeit) {
+        double annuität = darlehenssumme * (Math.pow(1+zinssatz, laufzeit) * (zinssatz / (Math.pow(1+zinssatz, laufzeit) - 1)));
+        return annuität;
+    }
+
+    public void onClick(View view){
+        double darlehenssumme = leseDarlehenssumme();
+        int zinssatz = leseZinssatz();
+        int laufzeit = leseLaufzeit();
+        double annuität = berechneAnnuität(darlehenssumme, zinssatz, laufzeit);
+        setContentView(R.layout.result_display);
+        TextView tVErgebnis = (TextView) findViewById(R.id.tVErgebnis);
+        tVErgebnis.setText(Double.toString(annuität));
     }
 
 }
