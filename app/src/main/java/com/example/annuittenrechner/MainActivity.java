1a9e0bb4a8b6e25a554064bcc70cfc8e1b8c335d
johannesrieder <johannesrieder13@hotmail.de>
2019-10-21 16:38:12 +0200

    Commit 21.10 Johannes Berechnung funktioniert

--- app/src/main/java/com/example/annuittenrechner/MainActivity.java
+++ app/src/main/java/com/example/annuittenrechner/MainActivity.java
@@ -12,10 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     Button AnnuitätButton;
+    Button BerechnungsverlaufButton;
     EditText eTDarlehenssumme;
     EditText eTZinssatz;
     EditText eTLaufzeit;
@@ -30,65 +31,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         AnnuitätButton = findViewById(R.id.AnnuitätButton);
         AnnuitätButton.setOnClickListener(this);
+        BerechnungsverlaufButton = findViewById(R.id.BerechnungsverlaufButton);
+        BerechnungsverlaufButton.setOnClickListener(this);
         eTDarlehenssumme = findViewById(R.id.eTDarlehenssumme);
         eTZinssatz = findViewById(R.id.eTZinssatz);
         eTLaufzeit = findViewById(R.id.eTLaufzeit);
     }
 
-    public double leseDarlehenssumme(){
+    public double leseDarlehenssumme() {
         double ds = Double.parseDouble(eTDarlehenssumme.getText().toString());
         return ds;
     }
 
-    public double leseZinssatz(){
+    public double leseZinssatz() {
         double zs = Double.parseDouble(eTZinssatz.getText().toString());
-        zs = zs/100;
+        zs = zs / 100;
         return zs;
     }
 
-    public int leseLaufzeit(){
+    public int leseLaufzeit() {
         int lz = Integer.parseInt(eTLaufzeit.getText().toString());
         return lz;
     }
 
     public double berechneAnnuität(double darlehenssumme, double zinssatz, int laufzeit) {
-        double annuität = darlehenssumme * (Math.pow(1+zinssatz, laufzeit) * (zinssatz / (Math.pow(1+zinssatz, laufzeit) - 1)));
-        annuität = Math.round(annuität*100.0)/100.0;
+        double annuität = darlehenssumme * (Math.pow(1 + zinssatz, laufzeit) * (zinssatz / (Math.pow(1 + zinssatz, laufzeit) - 1)));
+        annuität = Math.round(annuität * 100.0) / 100.0;
         return annuität;
     }
 
-    public void onClick(View view){
-        if (eTDarlehenssumme.getText().toString().isEmpty() == false && eTZinssatz.getText().toString().isEmpty() == false && eTLaufzeit.getText().toString().isEmpty() == false) {
-            double darlehenssumme = leseDarlehenssumme();
-            double zinssatz = leseZinssatz();
-            int laufzeit = leseLaufzeit();
-            double annuität = berechneAnnuität(darlehenssumme, zinssatz, laufzeit);
-            setContentView(R.layout.result_display);
-            tVBetrag = findViewById(R.id.tVDarlehenssumme);
-            tVZinssatz = findViewById(R.id.tVZinssatz);
-            tVErgebnis = findViewById(R.id.tVErgebnis);
-            tVLaufzeit = findViewById(R.id.tVLaufzeit);
-            tVErgebnis.setText(Double.toString(annuität));
-            tVBetrag.setText(Double.toString(darlehenssumme));
-            tVZinssatz.setText(Double.toString(zinssatz * 100));
-            tVLaufzeit.setText(Integer.toString(laufzeit));
-            System.out.println("Die Eingabe ist vollständig!");
-        } else {
-            System.out.println("Die Eingabe ist nicht vollständig!");
-            if (eTDarlehenssumme.getText().toString().isEmpty() == true){
-                eTDarlehenssumme.setHintTextColor(Color.RED);
-            }
-            if (eTZinssatz.getText().toString().isEmpty() == true){
-                eTZinssatz.setHintTextColor(Color.RED);
-            }
-            if (eTLaufzeit.getText().toString().isEmpty() == true){
-                eTLaufzeit.setHintTextColor(Color.RED);
+    public void onClick(View view) {
+        if (view == AnnuitätButton) {
+            if (eTDarlehenssumme.getText().toString().isEmpty() == false && eTZinssatz.getText().toString().isEmpty() == false && eTLaufzeit.getText().toString().isEmpty() == false) {
+                double darlehenssumme = leseDarlehenssumme();
+                double zinssatz = leseZinssatz();
+                int laufzeit = leseLaufzeit();
+                double annuität = berechneAnnuität(darlehenssumme, zinssatz, laufzeit);
+                setContentView(R.layout.result_display);
+                tVBetrag = findViewById(R.id.tVDarlehenssumme);
+                tVZinssatz = findViewById(R.id.tVZinssatz);
+                tVErgebnis = findViewById(R.id.tVErgebnis);
+                tVLaufzeit = findViewById(R.id.tVLaufzeit);
+                tVErgebnis.setText(Double.toString(annuität));
+                tVBetrag.setText(Double.toString(darlehenssumme));
+                tVZinssatz.setText(Double.toString(zinssatz * 100));
+                tVLaufzeit.setText(Integer.toString(laufzeit));
+                System.out.println("Die Eingabe ist vollständig!");
+            } else {
+                System.out.println("Die Eingabe ist nicht vollständig!");
+                if (eTDarlehenssumme.getText().toString().isEmpty() == true) {
+                    eTDarlehenssumme.setHintTextColor(Color.RED);
+                }
+                if (eTZinssatz.getText().toString().isEmpty() == true) {
+                    eTZinssatz.setHintTextColor(Color.RED);
+                }
+                if (eTLaufzeit.getText().toString().isEmpty() == true) {
+                    eTLaufzeit.setHintTextColor(Color.RED);
 
+                }
+                Toast laufzeitToast =
+                        Toast.makeText(this, "Eingaben unvollständig!", Toast.LENGTH_LONG);
+                laufzeitToast.show();
+                return;
             }
-            Toast laufzeitToast =
-                                    Toast.makeText(this, "Eingaben unvollständig!",Toast.LENGTH_LONG);
-                                    laufzeitToast.show();
-                                    return;
+        } else () {
+            setContentView(R.layout.verlauf_display);
         }
     }
 }
