bbe3c79de9c0cfaca4990f07d561c74e1294fe00
johannesrieder <Emil12041999>
2019-10-23 17:45:33 +0200

    Milestone

--- app/src/main/java/com/example/annuittenrechner/Hilfe.java
+++ app/src/main/java/com/example/annuittenrechner/Hilfe.java
@@ -2,13 +2,38 @@ package com.example.annuittenrechner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Hilfe extends AppCompatActivity {
+public class Hilfe extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button bStartseite;
+    Button BerechnungsverlaufButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hilfe_display);
+        bStartseite = findViewById(R.id.bStartseite);
+        BerechnungsverlaufButton2 = findViewById(R.id.BerechnungsverlaufButton2);
+
+        bStartseite.setOnClickListener(this);
+        BerechnungsverlaufButton2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == bStartseite){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+        if (view == BerechnungsverlaufButton2){
+            Intent intent = new Intent(this,Verlauf.class);
+            startActivity(intent);
+        }
     }
 }
