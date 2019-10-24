db621672d2a1e791c7ab1c4fe44501a889c23507
johannesrieder <Emil12041999>
2019-10-24 21:00:03 +0200

    24.10 Verlaufscreen

--- app/src/main/java/com/example/annuittenrechner/Verlauf.java
+++ app/src/main/java/com/example/annuittenrechner/Verlauf.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.List;
-public class Verlauf extends AppCompatActivity {
+public class Verlauf extends AppCompatActivity implements View.OnClickListener {
 
     private AnnuitaetDao dao;
     private RecyclerView recyclerView;
     private AnnuitaetListAdapter adapter;
+    Button HStartseiteButton;
+    ImageView iVhelpicon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +31,31 @@ public class Verlauf extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new AnnuitaetListAdapter(dao);
         recyclerView.setAdapter(adapter);
-
+        HStartseiteButton = findViewById(R.id.HStartseiteButton);
+        iVhelpicon = findViewById(R.id.iVhelpicon);
+        HStartseiteButton.setOnClickListener(this);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
         new LadeAnnuitaetsTask(dao,adapter).execute();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == HStartseiteButton){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+
+        if(view == iVhelpicon){
+            Intent intent = new Intent(this,Hilfe.class);
+            startActivity(intent);
+        }
+
+
     }
 
     static class LadeAnnuitaetsTask extends AsyncTask<Void, Void, List<Annuitaet>>{
